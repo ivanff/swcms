7d09debd36b6eb0236aa95c84468907b1b0f0a1d
ivan <agestart@gmail.com>
2018-09-04 10:51:55 +0300

    -

--- swcms_social/_utils/templatetags/_utils_tags.py
+++ swcms_social/_utils/templatetags/_utils_tags.py
@@ -1,6 +1,11 @@
 from django import template
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
+from django.contrib.messages import get_messages
 from django.urls.base import reverse
+
+import json
+
+from django.utils.html import escapejs
 
 register = template.Library()
 
@@ -16,3 +21,14 @@ def admin_url(obj, action='change'):
     else:
         raise ValueError
     return url
+
+
+@register.simple_tag(takes_context=True)
+def messages_to_json(context):
+    result = {}
+    messages = get_messages(context['request'])
+    for m in messages:
+        item = [{'message': m.message, 'level': m.level, 'extra_tags': m.extra_tags}]
+        if result.setdefault(m.tags, item) != item:
+            result[m.tags].extend(item)
+    return escapejs(json.dumps(result))
