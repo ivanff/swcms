20d121003243abcc26e41409bc44f1c0ef3c6c2a
ivan <agestart@gmail.com>
2021-01-17 12:57:50 +0300

    add snippet

--- swcms_social/text_pages/admin.py
+++ swcms_social/text_pages/admin.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from django.contrib import admin
 from .models import Pages, Snippet
 from swcms_social._utils.admin import BaseAdmin
@@ -10,9 +8,9 @@ class PagesAdmin(BaseAdmin):
     list_display = ('__str__', 'display_absolute_url', 'is_active', 'changed', 'created')
     list_filter = ('is_active',)
     
-    def __new__(cls) -> Any:
+    def __new__(cls, *args, **kwargs):
         cls.display_absolute_url.admin_order_field = 'url'
-        return super().__new__(cls)
+        return BaseAdmin.__new__(cls)
 
 
 @admin.register(Snippet)
